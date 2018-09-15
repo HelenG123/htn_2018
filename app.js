0af253e98f94422c9b6f7fbef8227163b428137c
jenip13 <32117180+jenip13@users.noreply.github.com>
2018-09-15 19:11:17 -0400

    test

---
+++ app.js
@@ -0,0 +1,13 @@
+import xesto from "xesto-wave-npm"
+const client = xesto( YOUR-API-KEY-HERE )
+
+client.connect().then( controller => {
+  //This is a Leap.Controller object, and we can pass it gesture names to have
+  //our app react to gestures!
+
+  controller.on("SwipeLeft", () => {
+    console.log("Woo! Swipe left!");
+  });
+
+  controller.connect();
+})
